3ec5331a45fda8ec515f5c4f8e7d7d799025e09d
Damian Oguche <damianguch@yahoo.com>
2025-02-07 12:40:36 +0100

    Updated the create contact modal to include twitter handle

--- src/components/contacts/CreateContactModal.jsx
+++ src/components/contacts/CreateContactModal.jsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import { Field } from '@/components/ui/field';
-import { Button } from '@/components/ui/button';
-import { Flex, Input, Stack, Textarea } from '@chakra-ui/react';
-import { BiAddToQueue } from 'react-icons/bi';
-import { CiImageOn } from 'react-icons/ci';
+import { Field } from "@/components/ui/field";
+import { Button } from "@/components/ui/button";
+import { Flex, Input, Stack, Textarea } from "@chakra-ui/react";
+import { BiAddToQueue } from "react-icons/bi";
+import { CiImageOn } from "react-icons/ci";
 import {
   DialogActionTrigger,
   DialogBody,
@@ -13,22 +13,23 @@ import {
   DialogHeader,
   DialogRoot,
   DialogTitle,
-  DialogTrigger
-} from '@/components/ui/dialog';
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
-import { useColorModeValue } from '@/components/ui/color-mode';
-import { useRef, useState } from 'react';
-import { toast } from 'react-toastify';
-import { BASE_URL } from '../../App';
+import { useColorModeValue } from "@/components/ui/color-mode";
+import { useRef, useState } from "react";
+import { toast } from "react-toastify";
+import { BASE_URL } from "../../App";
 
 const CreateContactModal = ({ setContacts }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [open, setOpen] = useState(false);
   const [inputs, setInputs] = useState({
-    name: '',
-    email: '',
-    address: '',
-    phone: ''
+    name: "",
+    email: "",
+    address: "",
+    phone: "",
+    xhandle: "",
   });
 
   const [selectedFile, setSelectedFile] = useState(null);
@@ -40,22 +41,23 @@ const CreateContactModal = ({ setContacts }) => {
 
     const formData = new FormData();
 
-    formData.append('name', inputs.name);
-    formData.append('email', inputs.email);
-    formData.append('phone', inputs.phone);
-    formData.append('address', inputs.address);
+    formData.append("name", inputs.name);
+    formData.append("email", inputs.email);
+    formData.append("phone", inputs.phone);
+    formData.append("address", inputs.address);
+    formData.append("xhandle", inputs.xhandle);
 
     if (selectedFile) {
-      formData.append('image', selectedFile);
+      formData.append("image", selectedFile);
     }
 
     try {
       const res = await fetch(`${BASE_URL}/api/contacts`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          Authorization: `Bearer ${sessionStorage.getItem('authToken')}`
+          Authorization: `Bearer ${sessionStorage.getItem("authToken")}`,
         },
-        body: formData
+        body: formData,
       });
 
       const result = await res.json();
@@ -65,7 +67,7 @@ const CreateContactModal = ({ setContacts }) => {
       }
 
       setContacts((prevContacts) => [...prevContacts, result.contact]);
-      setInputs({ name: '', email: '', address: '', phone: '' });
+      setInputs({ name: "", email: "", address: "", phone: "" });
       setSelectedFile(null);
       imgRef.current.value = null;
       toast.success(result.message);
@@ -94,19 +96,20 @@ const CreateContactModal = ({ setContacts }) => {
         </DialogTrigger>
         <DialogContent
           as="form"
-          bg={useColorModeValue('gray.100', 'gray.700')}
-          color={useColorModeValue('gray.900', 'white')}
-          onSubmit={handleCreateUser}>
+          bg={useColorModeValue("gray.100", "gray.700")}
+          color={useColorModeValue("gray.900", "white")}
+          onSubmit={handleCreateUser}
+        >
           <DialogHeader>
             <DialogTitle>Add Contact</DialogTitle>
           </DialogHeader>
           <DialogBody pb="4">
             <Stack gap="4">
-              <Flex alignItems={'center'} gap={4}>
+              <Flex alignItems={"center"} gap={4}>
                 <Field label="Full Name">
                   <Input
-                    bg={useColorModeValue('white', 'gray.800')}
-                    color={useColorModeValue('gray.900', 'white')}
+                    bg={useColorModeValue("white", "gray.800")}
+                    color={useColorModeValue("gray.900", "white")}
                     placeholder="John Doe"
                     value={inputs.name}
                     onChange={(e) =>
@@ -116,8 +119,8 @@ const CreateContactModal = ({ setContacts }) => {
                 </Field>
                 <Field label="Email">
                   <Input
-                    bg={useColorModeValue('white', 'gray.800')}
-                    color={useColorModeValue('gray.900', 'white')}
+                    bg={useColorModeValue("white", "gray.800")}
+                    color={useColorModeValue("gray.900", "white")}
                     placeholder="test@example.com"
                     value={inputs.email}
                     onChange={(e) =>
@@ -128,8 +131,8 @@ const CreateContactModal = ({ setContacts }) => {
 
                 <Field label="Phone">
                   <Input
-                    bg={useColorModeValue('white', 'gray.800')}
-                    color={useColorModeValue('gray.900', 'white')}
+                    bg={useColorModeValue("white", "gray.800")}
+                    color={useColorModeValue("gray.900", "white")}
                     placeholder="222555000"
                     value={inputs.phone}
                     onChange={(e) =>
@@ -137,11 +140,23 @@ const CreateContactModal = ({ setContacts }) => {
                     }
                   />
                 </Field>
+
+                <Field label="X-handle">
+                  <Input
+                    bg={useColorModeValue("white", "gray.800")}
+                    color={useColorModeValue("gray.900", "white")}
+                    placeholder="@ugbedam"
+                    value={inputs.xhandle}
+                    onChange={(e) =>
+                      setInputs({ ...inputs, xhandle: e.target.value })
+                    }
+                  />
+                </Field>
               </Flex>
               <Field label="Address">
                 <Textarea
-                  resize={'none'}
-                  overflowY={'hidden'}
+                  resize={"none"}
+                  overflowY={"hidden"}
                   placeholder="#345 Park Avenue, New York, USA"
                   value={inputs.address}
                   onChange={(e) =>
@@ -152,7 +167,7 @@ const CreateContactModal = ({ setContacts }) => {
 
               <Field label="Upload Image">
                 <CiImageOn
-                  cursor={'pointer'}
+                  cursor={"pointer"}
                   size={30}
                   onClick={() => imgRef.current.click()}
                 />
@@ -179,9 +194,10 @@ const CreateContactModal = ({ setContacts }) => {
               bg="cyan.400"
               type="submit"
               isDisabled={isLoading}
-              borderRadius={5}>
+              borderRadius={5}
+            >
               {isLoading ? (
-                <Flex justifyContent={'center'}>
+                <Flex justifyContent={"center"}>
                   <img
                     src="/spinner.gif"
                     alt="spinner"
@@ -190,7 +206,7 @@ const CreateContactModal = ({ setContacts }) => {
                   />
                 </Flex>
               ) : (
-                'Add'
+                "Add"
               )}
             </Button>
           </DialogFooter>
