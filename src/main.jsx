8cd4227748aef52deffac20629f3be75680b62ce
Damian Oguche <damianguch@yahoo.com>
2025-02-11 19:05:34 +0100

    CSS configuration for Inter google font

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import { Provider } from '@/components/ui/provider';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "../index.css";
+import { Provider } from "@/components/ui/provider";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
       <App />
